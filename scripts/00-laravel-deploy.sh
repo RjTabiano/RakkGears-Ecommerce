0f1c79b842014315b9942e7031187aad67f458e6
RjTabiano <tabianorj@gmail.com>
2025-08-19 17:28:28 +0800

    Added vite build in docker scripts

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
-echo "Running composer"
+
+# Install Node for Vite
+echo "Installing Node.js..."
+apt-get update && apt-get install -y nodejs npm
+
+# Composer install
+echo "Running composer..."
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
 
-echo "generating application key..."
+# Vite build
+echo "Installing npm dependencies..."
+cd /var/www/html
+npm install
+npm run build
+
+# Laravel setup
+echo "Generating application key..."
 php artisan key:generate --show
 
 echo "Caching config..."
